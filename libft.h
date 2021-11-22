3d4b6cc6425bcb2686e3358d61ce8bbe0e621998
ldubu <laura.dubuche@gamil.com>
2021-11-22 08:25:43 +0100

    second commit: libft.h modified, test and ft_putchar

--- libft.h
+++ libft.h
@@ -1,6 +1,6 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
-
+void	ft_putchar(char c);
 
 #endif
---
+++ test/test044_ft_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../libft.h"
+
+int	main(void)
+{
+	int	i;
+
+	i = 32;
+	while (i <= 127)
+		ft_putchar(i++);
+	return(0);
+}
