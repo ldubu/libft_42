c4cb04865991d790e838c7762ada5498910672a1
ldubu <laura.dubuche@gmail.com>
2021-11-24 21:10:46 +0100

    modif de strlcat de l'enfer, j'espere ça marchera demain

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,26 +14,25 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	s;
+	size_t	d;
 	size_t	i;
-	size_t	j;
-	size_t	size;
 
+	s = 0;
+	d = 0;
 	i = 0;
-	j = 0;
-	size = 40;
-	while (dst[i++] != '\0')
-		size++;
-	i = 0;
-	while (dst[i] && i < dstsize)
-		i++;
-	while (src[j] && i < dstsize)
-		dst[i++] = src[j++];
-	while (src[j])
-		j++;
-	if (dstsize != 0)
-		dst[i] = '\0';
-	//if (size < dstsize)
-	//	i = size;
-	printf("\ni%lu, j%lu size%lu\n", i, j, size);
-	return (j + i);
+	while (src[s])
+		s++;
+	while (dst[d])
+		d++;
+	if (d < dstsize)
+	{
+		while ((i + d) < dstsize)
+		{
+			dst[d + i] = src[i];
+			i++;
+		}
+		return (d + s);
+	}
+	return (dstsize + s);
 }
